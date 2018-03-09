df25951a538ed3efa6527aefcb5b82d9f12622cb
Andresf12 <landresf3638@gmail.com>
2018-03-09 16:46:55 -0600

    Tablero del juego

--- Game/game/src/App.js
+++ Game/game/src/App.js
@@ -1,67 +1,30 @@
-import React, { Component } from 'react';
-import GoogleLogin from 'react-google-login';
-import FacebookLogin from 'react-facebook-login';
-import  {Redirect} from 'react-router-dom';
-// or
-import { GoogleLogin } from 'react-google-login';
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import { IndexRoute, Router, Route, browserHistory } from 'react-router'
 
-import './App.css';
-import {PostData} from 'services/PostData';
+//App Layouts
+import Social from './components/social/'
+import DragDrop from './components/DragDrop/'
+import Jquery from './components/Jquery/'
+import Tablero from './components/social/tablero/'
+
+
+//Import css for the application
+import './style/index.css'
 
 class App extends Component {
-    constructor(props){
-        super(props);
-        this.stats={
-            redirectToReferrer:false
-        }
-        this.singup=this.singup.bind(this);
-    }
-    singup(res,type){
-        let PostData;
-        if(type==='facebook' && res.email){
-            PostData={name:res.name,provider:type,email:res.email,provider_id:res.id,token:res.accessToken,provider_pic:res.provider_pic}
-        }
-        if(type==='google' && res.w3.U3){
-            PostData={name:res.w3.id,provider:type,email:res.w3.U3,provider_id:res.El,token:res.Zi.access_token,provider_pic:res.w3.PAA}
-        }
-        PostData('login',this.state).then((result) => {
-            let responseJson = result;
-            if(responseJson.userData){
-                sessionStorage.setItem('userData',JSON.stringify(responseJson));
-                this.setState({redirectToReferrer: true});
-            }
-    });
-    }
-    render(){
-        if(this.state.redirectToReferrer){
-            return (<Redirect to={'/home'}/>);
-        }
-      const responseFacebook = (response) => {
-          console.log(response);
-          this.singup(response,'facebook')
-      }
-      const responseGoogle = (response) => {
-          console.log(response);
-          this.singup(response,'google')
-      }
+  render() {
     return (
-      <div className="App">
-        <header className="App-header">
-            <GoogleLogin
-                clientId="259726881032-iqe1f86o2nf9g9c38scng6ugpnkq23te.apps.googleusercontent.com"
-                buttonText="Login"
-                onSuccess={responseGoogle}
-                onFailure={responseGoogle}
-            />
-            <FacebookLogin
-                appId="185992235337870"
-                autoLoad={true}
-                fields="name,email,picture"
-                onClick={componentClicked}
-                callback={responseFacebook} />
-        </header>
-      </div>
+        <Router history={browserHistory}>
+            <Route path="/">
+                <IndexRoute title="Tutorial" component={Social} />
+            </Route>
+            <Route path="/social" component={Social}></Route>
+            <Route path="/dnd" component={DragDrop}></Route>
+            <Route path="/jquery" component={Jquery}></Route>
+            <Route path="/tablero" component={Tablero}></Route>
+        </Router>
     );
   }
 }
+
+export default App;
---
+++ Game/game/src/components/social/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+//Components
+import FacebookLogin from './FacebookLogin/'
+import GoogleLogin from './GoogleLogin/'
+import LinkedinLogin from './LinkedinLogin/'
+import Tablero from './tablero'
+//import config from '../../../config';
+class Social extends Component{
+    render(){
+        return(
+            <div>
+                    <Tablero/>
+            </div>
+        )
+    }
+}
+export default Social;
